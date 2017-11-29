d0bc9a0dda26204d9a89c3043439b07a78baa175
Andrew Henning-Kolberg (nm) <andrew.hk@nm.com>
2017-11-29 13:47:37 -0500

    refactored(?) some functions so they take arguments correctly

--- PythonTCEmailtoCRMParser/PythonTCEmailtoCRMParser.py
+++ PythonTCEmailtoCRMParser/PythonTCEmailtoCRMParser.py
@@ -8,16 +8,19 @@ TDD?
 '''
 
 import os
-import glob
+# import glob
 import xlrd
 # import re #I used string methods instead
 
-def transform_xl_to_list_of_dict():
+def transform_xl_to_list_of_dict(filename, header_row=9, data_row=10):
+    '''
+    takes the filename of the excel document to parse.
+    returns a list of dictionaries representing the worksheet.
+    Starts at the header row (defaults to row 9), and the data row is the next one (row 10)
+    '''
 
-    filename = 'C:\\Users\\perm7158\\Documents\\Projects\\Call RE Term Conversions\\Script\\06525_TC_1488954325929.xls'
-
-    header_row_in_worksheet = 9 # Header row is row 9 in the emailed spreadsheet
-    first_data_row_in_worksheet = 10 # First data row is right after that
+    header_row_in_worksheet = header_row # Header row is row 9 in the emailed spreadsheet
+    first_data_row_in_worksheet = data_row # First data row is right after that
 
     workbook = xlrd.open_workbook(filename)
     workbook = xlrd.open_workbook(filename, on_demand = True)
@@ -47,6 +50,10 @@ def importXLS(workbook):
 
 
 def output_list_of_insureds(xl_list_of_dict):
+    '''
+    Takes a list of dictionaries representing an excel spreadsheet
+    returns a list of the insureds names, in "First M  Last" format
+    '''
     list_of_insureds = [] # Blank list to hold the names of the insureds
     for row in xl_list_of_dict:
         name = row.get("Insured")
@@ -56,19 +63,30 @@ def output_list_of_insureds(xl_list_of_dict):
     return list_of_insureds
 
 def convert_fml_to_lcf(list_of_insureds):
+    '''
+    Takes a list of names in the format "First M  Last" and converts to "Last, First"
+    Returns that list
+    '''
+    list_of_insureds_lcf = []
     for name in list_of_insureds:
         double_space = len(name) - (name.find("  ")+2) 
         first_space = name.find(" ")
         first_name = name[0:first_space]
-        print('first_name, len: {}, {}'.format(first_name, len(first_name)))
+        #print('first_name, len: {}, {}'.format(first_name, len(first_name)))
         last_name = name[-double_space:]
-        print('last_name, len: {}, {}'.format(last_name, len(last_name)))
+        #print('last_name, len: {}, {}'.format(last_name, len(last_name)))
         lastcommafirst = last_name + ", " + first_name
         print(lastcommafirst)
+        list_of_insureds_lcf.append(lastcommafirst)
+
 
 if __name__ == "__main__":
+
+    #test file
+    filename = 'C:\\Users\\perm7158\\Documents\\Projects\\Call RE Term Conversions\\Script\\06525_TC_1488954325929.xls'
+
     # Script run thru debugger
-    xl_list_of_dict = transform_xl_to_list_of_dict()
+    xl_list_of_dict = transform_xl_to_list_of_dict(filename)
         
     # output a list of the insureds names
     list_of_insureds = output_list_of_insureds(xl_list_of_dict)
